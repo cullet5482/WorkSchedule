cb9529143be44c3fac860e51a8b7386d87ce12f4
cullet5482 <83200736+cullet5482@users.noreply.github.com>
2022-12-09 16:19:08 +0900

    Initial commit

---
+++ Date/Day.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkSchedule.Date
+{
+    public class DayData
+    {
+        public enum WorkType {Day = 0, Dayoff = -1, Night = 1}
+    }
+
+
+    public class Day
+    {
+        private bool indep;
+        private DateTime date;
+        private bool last;
+
+        public Day(Day day)
+        {
+            indep = day.indep;
+            date = day.date;
+            last = day.last;
+        }
+
+        public Day(DateTime date, bool indep, bool last)
+        {
+            this.date = date;
+            this.indep = indep;
+            this.last = last;
+
+        }
+    }
+}
